2e35cd1f9182725f79e3334913680f48ccba6439
khimsh <i.khimsh@gmail.com>
2021-12-19 19:35:08 +0400

    some js and responsive styles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,49 +9,51 @@ function pauseVideo(video) {
 }
 
 // scroll to top
-const scrollToTopButton = document.querySelector('[data-scroll-top]');
-scrollToTopButton.addEventListener('click', () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+if (document.querySelector('[data-scroll-top]')) {
+  const scrollToTopButton = document.querySelector('[data-scroll-top]');
+  scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+}
 
 // Sliders
-const featuredImage = new Swiper('.featured-image', {
-  direction: 'horizontal',
-  loop: true,
-  slidesPerView: 1,
-});
+// const featuredImage = new Swiper('.featured-image', {
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 1,
+// });
 
-const imageSwiper = new Swiper('.images', {
-  direction: 'horizontal',
-  loop: true,
-  slidesPerView: 2,
-  breakpoints: {
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    // when window width is >= 1200px
-    1200: {
-      slidesPerView: 5,
-      spaceBetween: 30,
-    },
-  },
+// const imageSwiper = new Swiper('.images', {
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 2,
+//   breakpoints: {
+//     // when window width is >= 640px
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 30,
+//     },
+//     // when window width is >= 992px
+//     992: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//     },
+//     // when window width is >= 1200px
+//     1200: {
+//       slidesPerView: 5,
+//       spaceBetween: 30,
+//     },
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
 
-featuredImage.controller.control = imageSwiper;
-imageSwiper.controller.control = featuredImage;
+// featuredImage.controller.control = imageSwiper;
+// imageSwiper.controller.control = featuredImage;
 
 // Tabs
 const tabs = document.querySelectorAll('[data-tab-target]');
@@ -70,3 +72,40 @@ tabs.forEach((tab) => {
     target.classList.add('active');
   });
 });
+
+// მობილური ნავიგაცია
+const openNavBtn = document.querySelector('[data-nav-open]');
+const closeNavBtn = document.querySelector('[data-nav-close]');
+const nav = document.querySelector('[data-navigation]');
+
+openNavBtn.addEventListener('click', () => {
+  nav.classList.add('active');
+});
+
+closeNavBtn.addEventListener('click', () => {
+  nav.classList.remove('active');
+});
+
+// Dropdowns
+if (document.querySelector('[data-trigger-included]')) {
+  const includedBtn = document.querySelector('[data-trigger-included]');
+  const includedTarget = document.querySelector('[data-target-included]');
+
+  includedBtn.addEventListener('click', () => {
+    includedTarget.classList.toggle('active');
+  });
+}
+
+// FAQS
+if (document.querySelectorAll('[data-faq-trigger]')) {
+  const faqTriggers = document.querySelectorAll('[data-faq-trigger]');
+
+  faqTriggers.forEach((faqTrigger) => {
+    faqTrigger.addEventListener('click', () => {
+      const faqContent = faqTrigger.nextElementSibling;
+
+      faqTrigger.classList.toggle('active');
+      faqContent.classList.toggle('active');
+    });
+  });
+}
